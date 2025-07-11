50e9f741e6c57a0625dee759d754e7c5838e63df
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-07-12 03:42:09 +0530

    remove: Redundant function for Enamoured badge

--- jobs/achievement.go
+++ jobs/achievement.go
@@ -138,23 +138,3 @@ func CheckFeature(username string) (int, bool, error) {
 	}
 	return count, true, nil
 }
-
-// -- Enamoured
-// Get a PR accepted every week for a month
-func CheckEnamoured(username string) (int, bool, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	countStr, err := cmd.Valkey.HGet(ctx, cmd.EnamouredSet, username).Result()
-	if err == redis.Nil {
-		return 0, false, nil
-	}
-	if err != nil {
-		return 0, false, err
-	}
-	count, err := strconv.Atoi(countStr)
-	if err != nil {
-		return 0, true, err
-	}
-	return count, true, nil
-}
