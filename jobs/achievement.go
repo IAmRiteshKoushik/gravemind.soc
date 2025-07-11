f68b000a476105e1f71cacb9163033169d5a01cf
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-07-12 02:32:27 +0530

    feat: Add EnamouredAchivement checking function

--- jobs/achievement.go
+++ jobs/achievement.go
@@ -141,6 +141,20 @@ func CheckFeature(username string) (int, bool, error) {
 
 // -- Enamoured
 // Get a PR accepted every week for a month
-func CheckEnamoured(username string) {
+func CheckEnamoured(username string) (int, bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
+	countStr, err := cmd.Valkey.HGet(ctx, cmd.EnamouredSet, username).Result()
+	if err == redis.Nil {
+		return 0, false, nil
+	}
+	if err != nil {
+		return 0, false, err
+	}
+	count, err := strconv.Atoi(countStr)
+	if err != nil {
+		return 0, true, err
+	}
+	return count, true, nil
 }
